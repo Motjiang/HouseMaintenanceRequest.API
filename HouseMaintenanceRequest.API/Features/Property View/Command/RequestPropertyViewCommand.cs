f54ba297996ebe7ee1e29222abb0a9fffca787e2
BrilliantMo <motjiangbrilliant@gmail.com>
2025-11-02 08:10:35 +0200

    Added the request property view command

---
+++ HouseMaintenanceRequest.API/Features/Property View/Command/RequestPropertyViewCommand.cs
@@ -0,0 +1,12 @@
+﻿using HouseMaintenanceRequest.API.Models.DTOs.Property_View;
+using MediatR;
+
+namespace HouseMaintenanceRequest.API.Features.Property_View.Command
+{
+    public record CreatePropertyViewRequestCommand(
+        int tenantId,
+        int propertyId,
+        DateTime scheduledAt
+    ) : IRequest<int>;
+}
+
