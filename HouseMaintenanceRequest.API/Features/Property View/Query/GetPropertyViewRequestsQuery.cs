5f7174f0a9f4675cb5cfa55876bc9c16eccb9a70
BrilliantMo <motjiangbrilliant@gmail.com>
2025-11-02 08:23:57 +0200

    Added the property view request Query  and handler

---
+++ HouseMaintenanceRequest.API/Features/Property View/Query/GetPropertyViewRequestsQuery.cs
@@ -0,0 +1,8 @@
+﻿using HouseMaintenanceRequest.API.Models.DTOs.Property_View;
+using MediatR;
+
+namespace HouseMaintenanceRequest.API.Features.Property_View.Query
+{
+    public record GetPropertyViewRequestsQuery()
+        : IRequest<List<PropertyViewRequestResponseDto>>;
+}
---
+++ HouseMaintenanceRequest.API/Models/DTOs/Property View/PropertyViewRequestResponseDto.cs
@@ -0,0 +1,17 @@
+﻿namespace HouseMaintenanceRequest.API.Models.DTOs.Property_View
+{
+    public class PropertyViewRequestResponseDto
+    {
+        public int ViewRequestId { get; set; }
+        public int TenantId { get; set; }
+        public string TenantName { get; set; }
+
+        public int PropertyId { get; set; }
+        public string PropertyName { get; set; }
+
+        public DateTime RequestedAt { get; set; }
+        public DateTime? ScheduledAt { get; set; }
+
+        public string Status { get; set; }
+    }
+}
